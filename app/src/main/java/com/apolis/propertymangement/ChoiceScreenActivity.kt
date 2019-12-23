5d07914dea7d33ea35a80f6fc61c6b7f9abd8a0f
Jorge <jorgexel@buffalo.edu>
2019-12-23 15:34:03 -0600

    Added files to start working for Choice Screen Activity

---
+++ app/src/main/java/com/apolis/propertymangement/ChoiceScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.apolis.propertymangement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChoiceScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choice_screen)
+    }
+}
