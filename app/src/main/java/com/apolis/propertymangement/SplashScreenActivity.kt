849061c940b9f89393d862748fa329a585c11873
yifanzhangeven <yzhang101@hawk.iit.edu>
2019-12-23 16:35:50 -0600

    branch test

--- app/src/main/java/com/apolis/propertymangement/SplashScreenActivity.kt
+++ app/src/main/java/com/apolis/propertymangement/SplashScreenActivity.kt
@@ -2,11 +2,14 @@ package com.apolis.propertymangement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
     }
 }
