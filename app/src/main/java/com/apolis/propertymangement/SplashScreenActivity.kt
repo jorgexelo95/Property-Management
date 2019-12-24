42b14694f932d277441244b2a24b2b3ab6dbe321
yifanzhangeven <yzhang101@hawk.iit.edu>
2019-12-23 21:18:39 -0600

    splash screen demo

--- app/src/main/java/com/apolis/propertymangement/SplashScreenActivity.kt
+++ app/src/main/java/com/apolis/propertymangement/SplashScreenActivity.kt
@@ -1,15 +1,33 @@
 package com.apolis.propertymangement
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var myHandler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        myHandler = Handler()
+
+        myHandler.postDelayed(
+            {
+                GoToActivity()
+            }, 3000
+        )
+
+    }
+
+    private fun GoToActivity() {
+
+        startActivity(Intent(this, ChoiceScreenActivity::class.java))
+
+        finish()
     }
 }
